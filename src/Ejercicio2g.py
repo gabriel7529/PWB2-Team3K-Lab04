c7e61f15985ef55fa42fda89ea1a3ac365f4c7c6
Edson López <elopezqu@unsa.edu.pe>
2022-05-30 20:56:53 -0500

    Se crea la primera fila de las piezas blancas

--- src/Ejercicio2g.py
+++ src/Ejercicio2g.py
@@ -1,2 +1,6 @@
 from interpreter import draw
 from chessPictures import *
+bloque1 = rock.join(knight).join(bishop)
+bloque2 = bloque1.verticalMirror()
+piezazBlancas = bloque1.join(queen).join(king).join(bloque2)
+draw(piezazBlancas)
