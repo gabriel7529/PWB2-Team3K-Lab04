ef5ed1e7040aa5be79a65e6db465698d12db26d5
Fernando Jesus Coyla Alvarez <fcoylaa@unsa.edu.pe>
2022-05-31 12:18:38 -0500

    ejercicio2a implementado usando metodos join y up

--- src/Ejercicio2a.py
+++ src/Ejercicio2a.py
@@ -1,2 +1,9 @@
-from interpreter import draw
 from chessPictures import *
+from interpreter import draw
+
+knight_black = knight.negative()
+
+fil1 = knight_black.join(knight)
+fil2 = knight.join(knight_black)
+
+draw(fil1.up(fil2))
