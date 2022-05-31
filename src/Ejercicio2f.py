a53ad35403ed79ed71384997ae03ed64fad0bc79
Edson López <elopezqu@unsa.edu.pe>
2022-05-30 20:20:27 -0500

    Se completa el ejercicio f

--- src/Ejercicio2f.py
+++ src/Ejercicio2f.py
@@ -1,2 +1,9 @@
 from interpreter import draw
 from chessPictures import *
+segmento1 = square.join(square.negative())
+segmento2 = square.negative().join(square)
+horizontal1 = segmento1.horizontalRepeat(3)
+horizontal2 = segmento2.horizontalRepeat(3)
+segmento3 = horizontal1.up(horizontal2)
+vertical = segmento3.verticalRepeat(3)
+draw(vertical)
